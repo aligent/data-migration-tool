c8c1f4fc6705a629e2e36d4a22b11159355e3dc0
Tom Zola <tom@alig1706.int.aligent.consulting>
2017-05-29 10:56:01 +0930

    custom translator for entity id fixed increments

--- src/Migration/Handler/ExistingDataSupport/EntityIdResolver.php
+++ src/Migration/Handler/ExistingDataSupport/EntityIdResolver.php
@@ -30,8 +30,15 @@ class EntityIdResolver extends \Migration\Handler\AbstractHandler implements \Mi
 
     //TODO MAKE DI FOR REUSE
     protected $incrementMap = [
-        'customer_entity.entity_id' => 660000000,
-        'customer_address_entity.entity_id' => 660000000
+        'customer_entity.entity_id' => 220000000,
+        'customer_address_entity.entity_id' => 330000000,
+        'newsletter_subscriber.subscriber_id' => 440000000
+    ];
+
+    protected $fkMap = [
+        'customer_entity.default_billing' => 'customer_address_entity.entity_id',
+        'customer_entity.default_shipping' => 'customer_address_entity.entity_id',
+        'customer_address_entity.parent_id' => 'customer_entity.entity_id'
     ];
 
     protected $relatedKey;
@@ -41,24 +48,43 @@ class EntityIdResolver extends \Migration\Handler\AbstractHandler implements \Mi
         $this->relatedKey = $relatedKey;
     }
 
+    public function getReverseIncrement($table, $field)
+    {
+        return -$this->getIncrement($table, $field);
+    }
+
+    public function getIncrement($table, $field)
+    {
+        $key = $table.'.'.$field;
+        if(isset($this->fkMap[$key])) {
+
+            $key = $this->fkMap[$key];
+        }
+        if(isset($this->incrementMap[$key])) {
+            return $this->incrementMap[$key];
+        }
+        return 0;
+    }
+
     /**
      * @inheritdoc
+     * todo refactor to use increment map instead of parameter.
      */
     public function handle(Record $recordToHandle, Record $oppositeRecord)
     {
         $this->validate($recordToHandle);
         $incrementBy = 0;
-        // if is a related key, lookup what the relative key has been implemented by
-        if($this->relatedKey && isset($this->incrementMap[$this->relatedKey])) {
-            $incrementBy = $this->incrementMap[$this->relatedKey];
-        }
-        // No related key given, and therefore is incrementing self
-        elseif (!$this->relatedKey && isset($this->incrementMap[$recordToHandle->getDocument()->getName().'.'.$this->field]))
-        {
-            $incrementBy = $this->incrementMap[$recordToHandle->getDocument()->getName().'.'.$this->field];
+
+        if($recordToHandle->getValue($this->field)) {
+            // if is a related key, lookup what the relative key has been implemented by
+            if ($this->relatedKey && isset($this->incrementMap[$this->relatedKey])) {
+                $incrementBy = $this->incrementMap[$this->relatedKey];
+            } // No related key given, and therefore is incrementing self
+            elseif (!$this->relatedKey && isset($this->incrementMap[$recordToHandle->getDocument()->getName() . '.' . $this->field])) {
+                $incrementBy = $this->incrementMap[$recordToHandle->getDocument()->getName() . '.' . $this->field];
+            }
         }
         //$oppositeRecord->setValue($this->field, $recordToHandle->getValue($this->field) + $incrementBy);
         $recordToHandle->setValue($this->field, $recordToHandle->getValue($this->field) + $incrementBy);
-
     }
 }
